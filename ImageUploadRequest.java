2385d68f235a0cea4d10c6053dfcf8af4f94653e
shubhamverma8577 <120180878+shubhamverma8577@users.noreply.github.com>
2024-06-02 18:32:34 +0530

    Add files via upload

---
+++ ImageUploadRequest.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.media.testing_proj.Bean;
+
+/**
+ *
+ * @author ShubhamVerma
+ */
+public class ImageUploadRequest {
+        private String imageFile;
+        private String userloginid;
+
+        public String getImageFile() {
+            return imageFile;
+        }
+
+        public void setImageFile(String imageFile) {
+            this.imageFile = imageFile;
+        }
+
+        public String getUserloginid() {
+            return userloginid;
+        }
+
+        public void setUserloginid(String userloginid) {
+            this.userloginid = userloginid;
+        }
+    }
+
+
